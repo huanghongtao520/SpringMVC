2748a753e9cdb33ced3f8c9b77e9a0451d406bc2
香蕉木头人520 <96758259+huanghongtao520@users.noreply.github.com>
2023-04-06 16:42:33 +0800

    12

--- A02-annotation/src/main/java/org/example/controller/HelloController.java
+++ A02-annotation/src/main/java/org/example/controller/HelloController.java
@@ -2,14 +2,20 @@ package org.example.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HelloController {
 
-    @RequestMapping("/hello")
-    public String hello(Model model){
-        model.addAttribute("msg","springmvc");
-        return "hello"; //会被视图解析器处理
+    // 传统风格：http://localhost:8082/add?a=1&b=1
+    // RestFul风格：使用@PathVariable注解,并修改路径为/add/{a}/{b}
+    // RestFul路径：http://localhost:8082/add/1/2
+    @RequestMapping("/add/{a}/{b}")
+    public String hello(@PathVariable int a, @PathVariable int b, Model model){
+
+        model.addAttribute("msg","结果为："+(a+b));
+
+        return "hello";
     }
 }
