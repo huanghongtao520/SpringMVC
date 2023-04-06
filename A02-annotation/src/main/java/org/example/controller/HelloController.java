715b12c95f3a421d4462f0e2b682f8addd8eaed1
香蕉木头人520 <96758259+huanghongtao520@users.noreply.github.com>
2023-04-06 15:53:02 +0800

    12

---
+++ A02-annotation/src/main/java/org/example/controller/HelloController.java
@@ -0,0 +1,15 @@
+package org.example.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloController {
+
+    @RequestMapping("/hello")
+    public String hello(Model model){
+        model.addAttribute("msg","springmvc");
+        return "hello"; //会被视图解析器处理
+    }
+}
